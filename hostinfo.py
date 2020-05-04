ac84b1d3c81b7b03e0bf19720a85da516a3bd6e2
Celticcow <gregory.dunlap@gmail.com>
2020-05-04 11:23:44 -0500

    corrected search of zone to zone and if fw or not by adding function to hostinfo

--- hostinfo.py
+++ hostinfo.py
@@ -28,12 +28,49 @@ class hostinfo(object):
 
     def get_count(self):
         return(self.count)
-
+    
+    def get_ip(self):
+        return(self.ip)
+        
     def get_meta(self, index):
         return(self.meta[index])
     
     def get_policy(self, index):
         return(self.policy[index])
     
-
+    def peer_zone(self, search_str, search_meta):
+        #dmz to zmd routed
+        if('Services-DMZ' in search_str):
+            for x in range(self.count):
+                if(self.policy[x] == 'services-zmd Security'):
+                    return True
+            return False
+        #zmd to dmz routed
+        elif('services-zmd' in search_str):
+            for x in range(self.count):
+                if(self.policy[x] == 'Services-DMZ Security'):
+                    return True
+            #return False
+        #zmd to zmd routed
+        #elif('services-zmd' in search_str):
+            for x in range(self.count):
+                if(self.policy[x] == 'services-zmd Security'):
+                    return True
+            return False
+        #DC Seg when in same dc
+        elif('DataCenterSeg' in search_str):
+            #my_meta_pre   = self.meta.split(' ')[0]
+            pass_meta_pre = search_meta.split(' ')[0]
+            for x in range(self.count):
+                my_meta_pre = self.meta[x].split(' ')[0]
+                if((self.policy[x] == 'DataCenterSeg Network') and (my_meta_pre == pass_meta_pre)):
+                    print(my_meta_pre + "  " + pass_meta_pre)
+                    return True
+            return False
+        ## more cases to come
+        #....
+        #Default return case
+        return False
+    #end of peer_zone()
+    
 #end of class hostinfo
--- is_needed.py
+++ is_needed.py
@@ -85,21 +85,52 @@ def policy_check(hostinfo1, hostinfo2, port):
             
     elif(host_len1 > 0 and host_len2 > 0):
         ### both searches returned hits.
-        # add 
-        for x in range(host_len1):
-            policylist.add(hostinfo1.get_policy(x))
-        
-        for y in range(host_len2):
-            policylist.add(hostinfo2.get_policy(y))
-        
+        # if dmz in a and zmd in b // exclude both
+        # if DataCenterSeg in both ... and both same DC exclude both
+
+        ###
+        # issue where zmd to zmd but wtc to edc.   so dc seg on one side
+        # so that still gets flagged ... soon wtc will have seg group though ... need special case ?
+        ###
+        for i in range(host_len1):
+            tmp_policy = hostinfo1.get_policy(i)
+            tmp_meta   = hostinfo1.get_meta(i)
+
+            if(hostinfo2.peer_zone(tmp_policy, tmp_meta)):
+                print("don't need to include : " + tmp_policy)
+            else:
+                policylist.add(hostinfo1.get_policy(i))
+            
+        for j in range(host_len2):
+            tmp_policy = hostinfo2.get_policy(j)
+            tmp_meta   = hostinfo2.get_meta(j)
+
+            if(hostinfo1.peer_zone(tmp_policy, tmp_meta)):
+                print("don't need to include : " + tmp_policy)
+            else:
+                policylist.add(hostinfo2.get_policy(j))
+
         print("-------------------------")
     else:
         print("Something went wrong with zone searches")
 
-    ### need logic here to weed out dmz to zmd and dc seg to dc seg if same data center
-
     print(policylist)
 #end of function  policy_check()
+
+def build_hostinfo(hostinfo, zone_list):
+    debug = 1
+
+    for x in zone_list:
+        if(x.compare(hostinfo.get_ip())):
+            if(debug == 1):
+                print("match in zone")
+                print(x.get_name())
+                print(x.get_meta())
+                print(x.get_policy())
+            hostinfo.add_info(x.get_meta().split(':')[1], x.get_policy().split(':')[1])
+    
+    return(hostinfo)
+
 
 """
 main function
@@ -112,14 +143,18 @@ def main():
 
     zones_list = build_zone_list()
 
-    ip1 = "204.135.8.10" #input("enter source IP address : ")
-    ip2 = "204.135.16.9" #input("enter destination IP address : ")
+    ip1 = "204.135.249.10" #input("enter source IP address : ")
+    ip2 = "204.135.8.16" #input("enter destination IP address : ")
     port = "9001" #input("enter port : ")
 
     hostinfo1 = hostinfo(ip1)
     hostinfo2 = hostinfo(ip2)
 
+    hostinfo1 = build_hostinfo(hostinfo1, zones_list)
+    print("**********************************")
+    hostinfo2 = build_hostinfo(hostinfo2, zones_list)
     #function later
+    """
     for x in zones_list:
         if(x.compare(ip1)):
             if(debug == 1):
@@ -131,6 +166,7 @@ def main():
             hostinfo1.add_info(x.get_meta().split(':')[1], x.get_policy().split(':')[1])
 
             print("----------------")
+    
     print("**********************************")
 
     for y in zones_list:
@@ -143,7 +179,8 @@ def main():
             #no labels on me
             hostinfo2.add_info(y.get_meta().split(':')[1], y.get_policy().split(':')[1])
             print("----------------")
-
+    # end function for laters
+    """
     policy_check(hostinfo1, hostinfo2, port)
  
     print("***** End of Program *****")
